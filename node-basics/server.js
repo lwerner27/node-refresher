7225439d9a36977563eb58b7b085b7fb6824e1d7
Lucas Werner <lwerner27@gmail.com>
2020-03-16 15:21:07 -0500

    Updated the node-basics project

--- node-basics/server.js
+++ node-basics/server.js
@@ -1,15 +1,38 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-	console.log(req.url, req.method, req.headers);
-	res.setHeader("contentType", "text/html");
-	res.write("<html>");
-	res.write("<head><title>Hello World</title></head>");
-	res.write("<body><h1>Hello World</h1></body>");
-	res.write("</html>");
-	res.end();
+    // console.log(req.url, req.method, req.headers);
+    const { url, method } = req;
+    if (url === "/") {
+        res.setHeader("contentType", "text/html");
+        res.write("<html>");
+        res.write("<head><title>Enter Message</title></head>");
+        res.write(
+            "<body><form action='/message' method='POST'><input name='message' type='text' /><button type='submit'>Send</button></form></body>"
+        );
+        res.write("</html>");
+        return res.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on("data", chunk => {
+            // console.log(chunk);
+            body.push(chunk);
+        });
+        req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            // console.log(message);
+            fs.writeFileSync("messages.txt", message);
+        });
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+    }
 });
 
 server.listen(4000, () => {
-	console.log("Listening on port: 4000");
+    console.log("Listening on port: 4000");
 });
