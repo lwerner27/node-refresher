25d1434a8be11c4ea8387dc8f770b5a7cf1e7eb5
Lucas Werner <lwerner27@gmail.com>
2020-03-18 16:14:44 -0500

    Updated some styles finished work on the dynamic rendering for the shop page.

--- express-basics/routes/shop.js
+++ express-basics/routes/shop.js
@@ -2,14 +2,14 @@ const path = require("path");
 const express = require("express");
 const rootDir = require("../utils/path");
 
-// const { products } = require("./admin");
+const { products } = require("./admin");
 
 const router = express.Router();
 
 // Default route.
 router.get("/", (req, res) => {
-    // console.log("shop.js", products);s
-    res.render("shop");
+    console.log(products);
+    res.render("shop", { products, docTitle: "Shop" });
 });
 
 module.exports = router;
