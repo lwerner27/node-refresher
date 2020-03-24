d53db080597c5347d95b542cf30ad6103758cfe2
Lucas Werner <lwerner27@gmail.com>
2020-03-24 16:19:49 -0500

    Refactored some of the code in the product modules to dry it up.

--- learn-mvc/models/product.js
+++ learn-mvc/models/product.js
@@ -1,6 +1,28 @@
 const fs = require("fs");
 const path = require("path");
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "products.json"
+);
+
+const getProductsFromFile = cb => {
+    // creates a string that represents the root directory.
+
+    // Gets data from the file if it exists
+    fs.readFile(p, (err, data) => {
+        // Executes if there is an error.
+        if (err) {
+            // Passes an empty array to the callback.
+            cb([]);
+        } else {
+            // Passes data to callback.
+            cb(JSON.parse(data));
+        }
+    });
+};
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
@@ -8,21 +30,7 @@ module.exports = class Product {
 
     // Method for saving new entries to the data file.
     save() {
-        // creates a string that represents the root directory.
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            "data",
-            "products.json"
-        );
-
-        // Checks for file and existing data.
-        fs.readFile(p, (err, data) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(data);
-            }
-
-            // Writes new data to the file.
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
                 if (err) {
@@ -34,23 +42,6 @@ module.exports = class Product {
 
     // Method for getting all products from the data file.
     static fetchAll(cb) {
-        // creates a string that represents the root directory.
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            "data",
-            "products.json"
-        );
-
-        // Gets data from the file if it exists
-        fs.readFile(p, (err, data) => {
-            // Executes if there is an error.
-            if (err) {
-                // Passes an empty array to the callback.
-                cb([]);
-            }
-
-            // Passes data to callback.
-            cb(JSON.parse(data));
-        });
+        getProductsFromFile(cb);
     }
 };
